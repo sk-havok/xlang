702b869ee3d1dc537d7668826d9f483e2a43e3c5
Kenny Kerr <kekerr@microsoft.com>
2018-10-15 15:51:11 -0700

    compare

---
+++ src/test/cpp/compare/project/Compare.Class.cpp
@@ -0,0 +1,9 @@
+﻿#include "pch.h"
+#include "Class.h"
+
+namespace winrt::Compare::implementation
+{    hstring Class::Method()
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ src/test/cpp/compare/project/Compare.Class.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Compare.Class.g.h"
+
+namespace winrt::Compare::implementation
+{
+    struct Class : ClassT<Class>
+    {
+        Class() = default;
+
+        hstring Method();
+    };
+}
+namespace winrt::Compare::factory_implementation
+{
+    struct Class : ClassT<Class, implementation::Class>
+    {
+    };
+}
