dace0bfea42c6db8b08f2f3a5215d8e03394156d
Kenny Kerr <kekerr@microsoft.com>
2018-10-16 09:32:00 -0700

    build

--- src/test/cpp/pch.h
+++ src/test/cpp/pch.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "catch.hpp"
+#include "winrt/base.h"
--- src/test/cpp/prebuild.bat
+++ src/test/cpp/prebuild.bat
@@ -3,3 +3,4 @@ midlrt attributes.idl /nomidl /winrt /winmd "Generated Files\attributes.winmd" /
 midlrt component.idl /nomidl /winrt /winmd "Generated Files\component.winmd" /metadata_dir C:\git\xlang\src\test\cpp /reference windows.winmd /reference "Generated Files\attributes.winmd" /h "nul"
 
 C:\git\xlang\_build\Windows\x86\Debug\tool\cpp\cpp.exe -in "Generated Files\component.winmd" -ref windows.winmd "Generated Files\attributes.winmd" -out "compare\out" -component "compare\project" -verbose -prefix -base -overwrite
+C:\git\xlang\_build\Windows\x86\Debug\tool\cpp\cpp.exe -in "Generated Files\component.winmd" -ref windows.winmd "Generated Files\attributes.winmd" -out "Generated Files" -component -verbose -prefix -base
